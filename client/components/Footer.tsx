0e0e042d50d59b4e036a6f0e6bbfeb4797f0c988
faelavgtt <rafavergottii@gmail.com>
2025-09-18 13:42:16 -0400

    teste

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -167,7 +167,7 @@ import { Button } from "@/components/ui/button";
                   </p>
 
                 </div>
-              </div><p>p</p>
+              </div>
             </div>
           </div>
         </div>
