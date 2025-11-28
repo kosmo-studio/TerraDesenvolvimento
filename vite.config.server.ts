fa4bf3ab0112c39bcdd34da4a4c3dfc46f422fd1
Diego Henrique Franco <diegohfranco@gmail.com>
2025-11-28 10:26:41 -0400

    Update vite.config.server.ts

--- vite.config.server.ts
+++ vite.config.server.ts
@@ -4,13 +4,14 @@ import path from "path";
 // Server build configuration
 export default defineConfig({
   build: {
+    base: "/",
     lib: {
       entry: path.resolve(__dirname, "server/node-build.ts"),
       name: "server",
       fileName: "production",
       formats: ["es"],
     },
-    outDir: "dist/server",
+    outDir: "dist/spa",
     target: "node22",
     ssr: true,
     rollupOptions: {
