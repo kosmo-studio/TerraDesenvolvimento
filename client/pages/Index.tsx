661946fe86e108dcda93e365651dca1cf54ebe12
Faelavgtt <rafavergottii@gmail.com>
2026-07-08 14:46:43 -0400

    formularios

--- client/pages/Index.tsx
+++ client/pages/Index.tsx
@@ -1,6 +1,5 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import { useState, useEffect } from 'react';
 import AnimatedSection from "@/components/AnimatedSection";
 import { PricingCardsContent } from "@/components/PrincingCardsContent";
 import HeroHome from "@/components/HeroHome";
@@ -11,37 +10,11 @@ import TestimonialsSection from "@/components/TestimonialsSection";
 import AboutSection from "@/components/AboutSection";
 import CtaSection from "@/components/CtaSection";
 import NewsSection from "@/components/NewsSection";
-import { Modal } from "@/components/Modal";
-import { CtaModalTerraVideo } from "@/components/CtaModel";
-import ContactPopup from "@/components/ContactPopup";
+import KommoLeadPopup from "@/components/KommoLeadPopup";
 
 export default function Index() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-    // 2. Efeito para disparar o modal ao carregar a página
-    useEffect(() => {
-        // Verifica se o usuário já viu o modal nesta sessão (sessionStorage)
-        const hasSeenModal = sessionStorage.getItem('blackFridayModalSeen');
-
-        if (!hasSeenModal) {
-            // Define um pequeno atraso de 1 segundo (1000ms)
-            const timer = setTimeout(() => {
-                setIsModalOpen(true);
-                // Marca o modal como visto
-                sessionStorage.setItem('blackFridayModalSeen', 'true');
-            }, 1000); 
-
-            return () => clearTimeout(timer); // Limpeza ao desmontar
-        }
-    }, []);
-
-    const handleCloseModal = () => setIsModalOpen(false);
   return (
     <div className="bg-white overflow-x-hidden">
-      <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
-                {/* O conteúdo da sua oferta */}
-                <CtaModalTerraVideo onClose={handleCloseModal} />
-            </Modal>
       <Header />
       
       <main>
@@ -78,7 +51,7 @@ export default function Index() {
           <NewsSection />
         </AnimatedSection>
       </main>
-      <ContactPopup />
+      <KommoLeadPopup />
       <Footer />
     </div>
   );
